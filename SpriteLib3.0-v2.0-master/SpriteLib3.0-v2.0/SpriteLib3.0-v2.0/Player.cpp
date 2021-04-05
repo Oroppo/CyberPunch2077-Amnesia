3e62be0b79c0ced4f3703c9e54ab62f486e2ebc9
Ryan <rcfieldhouse@yahoo.com>
2021-04-05 10:56:18 -0400

    literally just comments

--- SpriteLib3.0-v2.0-master/SpriteLib3.0-v2.0/SpriteLib3.0-v2.0/Player.cpp
+++ SpriteLib3.0-v2.0-master/SpriteLib3.0-v2.0/SpriteLib3.0-v2.0/Player.cpp
@@ -162,6 +162,8 @@ void Player::MovementUpdate()
 	// @Ryan i commented out ur true/false cout just uncomment to get them back
 
 	//std::cout << " available: " << std::boolalpha << impactAvailable << " Y: " << impactY << " X: " << impactX << "\n";
+
+	//jump function
 	if (canJump.m_canJump == true)
 	{
 		if (impactAvailable == true) {
@@ -207,7 +209,7 @@ void Player::MovementUpdate()
 	}
 
 
-
+	//dash function 
 	if (dash == true) {
 		if (canJump.m_canJump == true) {
 
